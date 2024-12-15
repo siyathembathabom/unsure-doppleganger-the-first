093998757378db7baa5dc0d56e27dbaaef3114ba
siyathembathabom <siyathembathabom@duck.com>
2024-12-15 20:19:19 +0200

    Added skeleton code for UserCredentials & Accout, with skeleton for tests

---
+++ common/src/test/java/src/test/java/co/za/share/AccountTest.java
@@ -0,0 +1,22 @@
+package src.test.java.co.za.share;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+public class AccountTest {
+
+    @Test
+    public void testGetUserId() {
+
+    }
+
+    @Test
+    public void testGetUserBankName() {
+
+    }
+
+    @Test
+    public void testGetUserAccountNumber() {
+
+    }
+}
---
+++ common/src/test/java/src/test/java/co/za/share/UserCredentialsTest.java
@@ -0,0 +1,32 @@
+package src.test.java.co.za.share;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+public class UserCredentialsTest {
+    
+    @Test
+    public void testGetUserIdentifier() {
+        
+    }
+
+    @Test
+    public void testGetUserName() {
+
+    }
+
+    @Test
+    public void testGetUserEmail() {
+
+    }
+
+    @Test
+    public void testGetUserPhoneNumber() {
+
+    }
+
+    @Test
+    public void testGetUserAccountBalance() {
+
+    }
+}
