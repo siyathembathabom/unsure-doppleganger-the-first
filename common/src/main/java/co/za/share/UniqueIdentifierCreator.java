07a5e5f48c957d3a2a6bc157d6548b50721b0d5a
siyathembathabom <siyathembathabom@duck.com>
2024-12-18 19:55:43 +0200

    Updated UniqueIdentifierCreator class, added constructor and createUserID methods to create a new user id and tests

--- common/src/main/java/co/za/share/UniqueIdentifierCreator.java
+++ common/src/main/java/co/za/share/UniqueIdentifierCreator.java
@@ -1,5 +1,20 @@
 package co.za.share;
 
-public class UniqueIdentifierCreator {
+import java.security.SecureRandom;
 
+public class UniqueIdentifierCreator {
+    private String uniqueID;
+    private SecureRandom secureRandom;
+
+    public UniqueIdentifierCreator() {
+        this.uniqueID = "#-195-";
+        this.secureRandom = new SecureRandom();
+    }
+
+    public String createUserID() {
+        for (int i = 0; i < 4; i++) {
+            this.uniqueID += this.secureRandom.nextInt(9);
+        }
+        return this.uniqueID;
+    }
 }
---
+++ common/src/test/java/src/test/java/co/za/share/UniqueIdentifierCreatorTest.java
@@ -0,0 +1,26 @@
+package src.test.java.co.za.share;
+
+import co.za.share.UniqueIdentifierCreator;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class UniqueIdentifierCreatorTest {
+
+    @Test
+    public void testNoUniqueIDShouldBeTheSame() {
+        UniqueIdentifierCreator uniqueIdentifierCreator1 = new UniqueIdentifierCreator();
+        UniqueIdentifierCreator uniqueIdentifierCreator2 = new UniqueIdentifierCreator();
+        assertNotEquals(uniqueIdentifierCreator1.createUserID(), 
+            uniqueIdentifierCreator2.createUserID());
+    }
+
+    @Test
+    public void testContainsIdentifierKey() {
+        UniqueIdentifierCreator uniqueIdentifierCreator = new UniqueIdentifierCreator();
+        String userID = uniqueIdentifierCreator.createUserID();
+        assertTrue(userID.contains("#-195-"));
+    }
+}
